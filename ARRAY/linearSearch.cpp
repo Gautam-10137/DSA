6ad7b5ff34acd5068468cb5fbce8b9e78fe482c2
Gautam-10137 <112772090+Gautam-10137@users.noreply.github.com>
2022-09-14 17:50:24 +0530

    first commit

---
+++ ARRAY/linearSearch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int linearSearch(int arr[],int n , int key){
+ 
+     for(int i=0;i<n;i++){
+         if(arr[i]==key){
+             return i;
+         }
+  }
+     return -1;
+   
+}
+int main(){
+
+    int n,key;
+    cin>>n;
+    cout<<"ENter the Numbers"<<endl;
+    int arr[n];
+    for(int i=0;i<n;i++){
+       cin>>arr[i];
+    }
+    cout<<"Enter the Key"<<endl;
+    cin>>key;
+    cout<<linearSearch(arr,n,key);
+    
+
+
+
+
+
+
+    return 0;
+}
